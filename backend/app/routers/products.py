f3f336d13e95ddfe7a47cfc6a74faf3278c21663
Drixner <drixnerrafael1@gmail.com>
2024-01-24 21:34:25 -0500

    Delete product routes

---
+++ backend/app/routers/products.py
@@ -0,0 +1,69 @@
+"""
+Este modulo sirve para definir las rutas de los productos"""
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from .. import schemas, models
+from ..database import SessionLocal
+
+router = APIRouter()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/", response_model=schemas.Product)
+def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
+    db_product = models.Product(**product.dict())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+
+@router.get("/", response_model=List[schemas.Product])
+def read_products(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    products = db.query(models.Product).offset(skip).limit(limit).all()
+    return products
+
+
+@router.get("/{product_id}", response_model=schemas.Product)
+def read_product(product_id: int, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == product_id).first()
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
+
+
+@router.put("/{product_id}", response_model=schemas.Product)
+def update_product(
+    product_id: int, product: schemas.ProductCreate, db: Session = Depends(get_db)
+):
+    db_product = (
+        db.query(models.Product).filter(models.Product.id == product_id).first()
+    )
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    for key, value in product.dict().items():
+        setattr(db_product, key, value)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+
+@router.delete("/{product_id}")
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    db_product = (
+        db.query(models.Product).filter(models.Product.id == product_id).first()
+    )
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    db.delete(db_product)
+    db.commit()
+    return {"detail": "Product deleted"}
